cc77b74baae47a649b8fd6668e5717a51ad348d5
zwfun <969748465@qq.com>
2020-08-11 09:10:24 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '凡夫俗子基础知识学习',
+    description: '凡夫俗子基础知识学习',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated'
+    }
+}
